a2bd571f5edb8613802b38176a0bbd5e1a54fe52
Mlkagaxanli <melik.agaxanli@gmail.com>
2021-11-07 03:21:12 +0400

    added entity package

--- src/main/java/com/developia/booksforeverybody/dao/entity/UserEntity.java
+++ src/main/java/com/developia/booksforeverybody/dao/entity/UserEntity.java
@@ -3,8 +3,11 @@ package com.developia.booksforeverybody.dao.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,11 +24,16 @@ public class UserEntity {
     @Column(name = "id")
     private Long id;
 
+    @NotBlank
+    @Length(min = 3, max = 20)
     @Column(name = "first_name")
     private String firstName;
 
+    @NotBlank
+    @Length(min = 5, max = 25)
     @Column(name = "last_name")
     private String lastName;
+
 
     @Column(name = "address")
     private String address;
@@ -34,15 +42,22 @@ public class UserEntity {
     @Enumerated(value = EnumType.STRING)
     private UserStatus status;
 
+    @NotBlank
+    @Length(min = 9, max = 11)
     @Column(name = "phone_number")
     private String phoneNumber;
 
+    @Email
     @Column(name = "email")
     private String  email;
 
+    @NotBlank
+    @Length(min = 5, max = 20)
     @Column(name = "username")
     private String  username;
 
+    @NotBlank
+    @Length(min = 8, max = 20)
     @Column(name = "password")
     private String  password;
 

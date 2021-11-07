f298c338b37e8b4ecd5058cd752fdd239c4cb3c8
Mlkagaxanli <melik.agaxanli@gmail.com>
2021-11-08 02:07:33 +0400

    implemented get cart by username

---
+++ src/main/java/com/developia/booksforeverybody/service/CartService.java
@@ -0,0 +1,7 @@
+package com.developia.booksforeverybody.service;
+
+import com.developia.booksforeverybody.dao.entity.CartEntity;
+
+public interface CartService {
+    CartEntity getCart(String username);
+}
